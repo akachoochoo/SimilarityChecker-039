606bd3171cd28f74509e427ef27d46762ad92c20
changlae choo <cl.choo@samsung.com>
2025-05-07 10:12:48 +0900

    Refactor-1

--- SimilarityChecker/similaritychecker.cpp
+++ SimilarityChecker/similaritychecker.cpp
@@ -4,12 +4,22 @@ using std::string;
 class SimilarityChecker {
 public:
 	int getSimilarity(string& input1, string& input2)
-	{
-		if (input1.length() > input2.length())
-			if (input1.length() > (input2.length() * 2)) return 0;
-		if (input1.length() < input2.length())
-			if (input1.length() < (input2.length() * 2)) return 0;
+	{		
+		if (isMax(input1, input2)) return LengthMaxScore;
 
-		return 60;
+		return LengthMinScore;
 	}
+
+	bool isMax(std::string& input1, std::string& input2)
+	{
+		bool retFlag = true;
+
+		int length1 = input1.length();
+		int length2 = input2.length();
+
+		if (length1 == length2) return true;		
+	}
+private:
+	static constexpr int LengthMaxScore = 60;
+	static constexpr int LengthMinScore = 0;
 };
